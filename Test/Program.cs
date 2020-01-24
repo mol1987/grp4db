aa1cbd5183fb7c443fa3ab31f5a2519124ee29c9
jakob andersson <jakob.aah@gmail.com>
2020-01-24 16:38:47 +0100

    Large - Moved physical files into lib/ to match Visual Studio Structure - Added Tests - Add Env-Loader and some tests for it - Global Setting class - Updated Fredriks connection-string

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MsSqlRepo;
 using System;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using TypeLib;
 
@@ -8,6 +10,61 @@ namespace Test
     class Program
     {
         static async Task Main(string[] args)
+        {
+            int i = 0; // Increment
+            int n = 2; // Number of test
+            Console.Write("Running {0} tests\n", n);
+
+            //test1 - Check if .env exists and is loaded
+            string res = Helper.Environment.LoadEnvFile() ? "true" : "false";
+            if (res == "true")
+            {
+                Console.BackgroundColor = ConsoleColor.DarkCyan;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+            }
+            Console.Write("Loaded env = {0}\n", res);
+            Console.ResetColor();
+            i++;
+
+            //test1b Check if global values are set
+            try
+            {
+                // key => value
+                string[] values = new string[]
+                {
+                    "host",Helper.Globals.Get("host"),
+                    "db",Helper.Globals.Get("db"),
+                    "usr",Helper.Globals.Get("usr"),
+                    "pwd",Helper.Globals.Get("pwd"),
+                    "port",Helper.Globals.Get("port"),
+                };
+
+                for(int j = 0; j < values.Length; j += 2)
+                {
+                    Console.Write("{0} => {1}\n", values[j], values[j + 1]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.Write("Loaded env = {0}\n", res);
+                Console.ResetColor();
+            }
+
+
+            //test2
+            await Test2();
+            i++;
+
+            Console.WriteLine("Press any key to quit..");
+            Console.ReadKey();
+            System.Environment.Exit(1);
+        }
+
+        static async Task Test2()
         {
             var repo = new Repository();
 
@@ -20,8 +77,6 @@ namespace Test
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
         }
     }
 }
---
+++ lib/Helper/Color.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+    public class Color
+    {
+        public class ForeGround
+        {
+            public static string Black = "\u001b[30m";
+            public static string Red = "\u001b[31m";
+            public static string Green = "\u001b[32m";
+            public static string Yellow = "\u001b[33m";
+            public static string Blue = "\u001b[34m";
+            public static string Magenta = "\u001b[35m";
+            public static string Cyan = "\u001b[36m";
+            public static string White = "\u001b[37m";
+        }
+        public class BackGround
+        {
+            public static string Black = "\u001b[40m";
+            public static string Red = "\u001b[41m";
+            public static string Green = "\u001b[42m";
+            public static string Yellow = "\u001b[43m";
+            public static string Blue = "\u001b[44m";
+            public static string Magenta = "\u001b[45m";
+            public static string Cyan = "\u001b[46m";
+            public static string White = "\u001b[47m";
+        }
+        public static string Bright = ";1m";
+        public static string Reset = "\u001b[0m";
+    }
+}
