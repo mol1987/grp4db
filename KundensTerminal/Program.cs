c92921126df1592aa2d1cf0dd3839b42284246a5
mol1987 <57093339+mol1987@users.noreply.github.com>
2020-02-12 20:46:45 +0100

    Kundterminalen är lite bättre formaterad och la till en pågånde lista

--- KundensTerminal/Program.cs
+++ KundensTerminal/Program.cs
@@ -11,9 +11,12 @@ namespace KundensTerminal
 {
     class Program
     {
-        
+        static int origRow, origCol;
         static async Task Main(string[] args)
-        {// kund terminal 
+        {
+            origRow = Console.CursorTop;
+            origCol = Console.CursorLeft;
+            // kund terminal 
             /// Change the Window titles
             Console.Title = "Kund";
             bool res = Helper.Environment.LoadEnvFile() ? true : false;
@@ -25,12 +28,33 @@ namespace KundensTerminal
                 
                 List<Orders> allOrders = (await General.ordersRepo.GetAllAsync()).ToList();
                 List<Orders> allDoneOrders = allOrders.Where(x => x.Orderstatus == 2).ToList();
+                List<Orders> allProcessing = allOrders.Where(x => x.Orderstatus == 1).ToList();
                 thr.Interrupt();
                 Console.Clear();
-                allDoneOrders.ForEach(x => Console.WriteLine(x.ID + " " + x.TimeCreated));
+                int cx = 0, cy = 2;
+                Console.WriteLine("Pågående ordrar | Klara ordrar");
+                Console.WriteLine("-------------------------------");
+                allProcessing.ForEach(x => WriteAt("" + x.ID, 5, cy++));
+                cy = 2;
+                allProcessing.ForEach(x => WriteAt("|", 16, cy++));
+                cy = 2;
+                allDoneOrders.ForEach(x => WriteAt("" + x.ID, 23, cy++));
                 System.Threading.Thread.Sleep(2000);
             }
+        }
 
+        static void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
         }
 
         static void loading()
