803f5758a22c831982f30c644aa5b815acbb6971
Firashanna <56591432+Firashanna@users.noreply.github.com>
2020-02-12 22:55:10 +0100

    Lite ändring

--- KockTerminal_JC/Program.cs
+++ KockTerminal_JC/Program.cs
@@ -22,15 +22,16 @@ namespace KockTerminal_JC
 
 
                 List<Orders> beginningOrder = allOrders.Where(x => x.Orderstatus == 0).ToList();
-                Console.WriteLine("välig den order som du vill tillaga");
+                Console.WriteLine("välje den order som du vill tillaga");
 
                 Console.WriteLine(" ");
                 foreach (Orders orderItem in beginningOrder)
                 {
                     orderItem.Articles = (await General.articlesRepo.GetAllAsync(orderItem)).ToList();
                     Console.Write(orderItem.ID + " ");
+                    
                     orderItem.Articles.ForEach(x => Console.WriteLine(x.Name));
-
+                    
                 }
 
                 int choice = 62;
@@ -41,7 +42,7 @@ namespace KockTerminal_JC
                     if (items.ID == choice)
                     {
                         Console.WriteLine(" ");
-                        Console.WriteLine(" den order som du väljat inhåller följande ingredienter");
+                        Console.WriteLine("Den order som du valt inhåller följande ingredienser");
                         Console.WriteLine(" ");
                         foreach (var item in items.Articles)
                         {
@@ -62,13 +63,13 @@ namespace KockTerminal_JC
                 }
                 Console.WriteLine("--------------");
                 Console.WriteLine(" ");
-                Console.WriteLine(" välja 1: för stopa in i ugn " + "\n" + "välja 2:Återgå");
+                Console.WriteLine(" välja 1: för stopa in i ugnen " + "\n" + "välja 2: Återgå");
                 int selection = Convert.ToInt32(Console.ReadLine());
                 if (selection == 1)
                 {
                     List<Orders> allDoneOrders = allOrders.Where(x => x.Orderstatus == 2).ToList();
                     Console.WriteLine(" ");
-                    Console.WriteLine(" foljande orderar finns redo att hämta ut");
+                    Console.WriteLine(" foljande orderar finns redo att hämtas ut");
 
                     allDoneOrders.ForEach(x => Console.WriteLine(x.ID + " " + "\n" + "------"));
                     Console.WriteLine(" Ange numret för den order som  gästen  hämtat");
@@ -137,5 +138,7 @@ namespace KockTerminal_JC
             //    }
 
         }
+
+        
     }
 }
